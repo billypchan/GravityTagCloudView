85c656fadd40047c290597c27543b37319ff38fd
Bill Chan MBA <billypchan@gmail.com>
2017-02-26 12:57:30 +0100

    Initial Commit

---
+++ GravityTagCloudView/GravityTagCloudView.h
@@ -0,0 +1,19 @@
+//
+//  GravityTagCloudView.h
+//  GravityTagCloudView
+//
+//  Created by chan bill on 26/2/2017.
+//  Copyright © 2017 chan bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GravityTagCloudView.
+FOUNDATION_EXPORT double GravityTagCloudViewVersionNumber;
+
+//! Project version string for GravityTagCloudView.
+FOUNDATION_EXPORT const unsigned char GravityTagCloudViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GravityTagCloudView/PublicHeader.h>
+
+
